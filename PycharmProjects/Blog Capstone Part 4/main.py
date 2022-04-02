ba14cbc8c5b4bd1062095ebe0f5d46d1a7339fa4
andriyenkoa <andrienko-a7@mail.ru>
2022-04-02 12:52:11 +0300

    Add gunicorn

--- PycharmProjects/Blog Capstone Part 4/main.py
+++ PycharmProjects/Blog Capstone Part 4/main.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, redirect, url_for, flash, abort, request
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from datetime import date
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
+from flask_gravatar import Gravatar
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -203,3 +214,4 @@ def load_user(user_id):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
